be457c50e0421e950d612e4e71ea0a697384e141
Cao007 <1114685490@qq.com>
2025-01-08 01:08:34 +0800

    fix:babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "node": true
   },
   "extends": [
-    "standard",
+    // "standard",
     "plugin:vue/strongly-recommended"
   ],
   "parserOptions": {
--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -13,6 +13,11 @@ module.exports = {
 
   module: {
     rules: [
+      // css
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
       // less
       {
         test: /\.less$/,
